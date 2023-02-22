4eebe9edc782d29f43ee761a22db039d62683844
Laurence Trippen <laurence.trippen@smartelephantapps.com>
2023-02-22 22:49:59 +0400

    SDL2 window & renderer (accelerated)

--- src/main.c
+++ src/main.c
@@ -1,34 +1,53 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
 
 int main(int argc, char* args[])
 {
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
-		return 1;
+	// Window
+	SDL_Window* window = SDL_CreateWindow("RogueLike", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, NULL);
+	if (!window)
+	{
+		SDL_Log("Could not create a window: %s", SDL_GetError());
+		return -1;
 	}
-	window = SDL_CreateWindow(
-		"hello_sdl2",
-		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-		SCREEN_WIDTH, SCREEN_HEIGHT,
-		SDL_WINDOW_SHOWN
-	);
-	if (window == NULL) {
-		fprintf(stderr, "could not create window: %s\n", SDL_GetError());
-		return 1;
+
+	// Renderer
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (!renderer)
+	{
+		SDL_Log("Could not create a renderer: %s", SDL_GetError());
+		return -1;
 	}
-	screenSurface = SDL_GetWindowSurface(window);
-	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-	SDL_UpdateWindowSurface(window);
-	SDL_Delay(2000);
+
+	// Game Loop
+	bool done = false;
+	while (!done)
+	{
+		// Get the next event
+		SDL_Event event;
+		if (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				done = true;
+			}
+		}
+
+		// Set the color to cornflower blue and clear
+		SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
+		SDL_RenderClear(renderer);
+
+		// Show the renderer contents
+		SDL_RenderPresent(renderer);
+	}
+
+	// Cleanup
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+
 	return 0;
 }
